60af59721ced9e679ddfd30524b08f4399e9e4da
tomelin07 <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>
2025-09-21 22:42:43 -0300

    feat: adicionar subclasse Cachorro com atributo raca e sobrescrita de emitirSom()

---
+++ src/main/java/br/edu/projeto_heranca/modelo/Cachorro.java
@@ -0,0 +1,30 @@
+public class Cachorro extends Animal {
+    private String raca;
+
+    public Cachorro(String nome, int idade, String raca) {
+        super(nome, idade);
+        this.raca = raca;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    @Override
+    public String emitirSom() {
+        return "O cachorro late: Au Au!";
+    }
+
+    @Override
+    public String toString() {
+        return "Cachorro{" +
+                "nome='" + getNome() + '\'' +
+                ", idade=" + getIdade() +
+                ", raca='" + raca + '\'' +
+                '}';
+    }
+}
